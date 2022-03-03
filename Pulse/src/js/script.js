ec51f40e25b67ab8ff83fea91cd3b2a528d5c898
Mikhail <mikhailkhudoshin@gmail.com>
2022-03-04 03:05:09 +0400

    feat: add modal windows

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //Slider
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -6,6 +7,7 @@ $(document).ready(function() {
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
     });
 
+    //Tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -24,4 +26,20 @@ $(document).ready(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
 });
