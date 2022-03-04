114bfff447bb5f58e1216e390c84194a34e35878
Mikhail <mikhailkhudoshin@gmail.com>
2022-03-04 19:56:44 +0400

    feat: add scrollTop

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -100,4 +100,18 @@ $(document).ready(function() {
         });
         return false;
     });
+
+    //Smoth scroll and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top + "px"});
+    });
 });
